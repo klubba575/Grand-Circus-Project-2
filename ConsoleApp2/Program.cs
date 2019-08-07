0cdc5aa5098c36f8f786aacc5aef1496c8518a57
klubba575 <wilderandrew9@gmail.com>
2019-08-07 15:49:50 -0400

    first commit GC

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,57 @@ namespace ConsoleApp2
 	{
 		static void Main(string[] args)
 		{
-		}
+			Console.WriteLine("  Let's Figure Out Your Plan For This Weekend, Shall We?");
+			Console.WriteLine("  What type of activity are you in the mood for?");
+			Console.WriteLine("  Please select the number based on the matching mood! ");
+			Console.WriteLine("   1 for Action, 2 for Chilling, 3 for Danger, and 4 for Good Food");
+			string mood = (Console.ReadLine());
+
+			Console.WriteLine("And how many companions will be joining you on this adventure?");
+			Console.WriteLine("Press 1 for 0 companions, 2 for 1-4, 3 for 5-10, and 4 for 11+ companions");
+			string companions = Console.ReadLine();
+
+
+			if (mood == "1")
+				{
+					Console.WriteLine("You are in the mood for Action, you should go Stock Car Racing");
+				}
+				else if (mood == "2")
+				{
+					Console.WriteLine("You are in the mood for Chilling, you should go hiking"); ;
+				}
+				else if (mood == "3")
+				{
+				Console.WriteLine("You are in the mood for Danger, you should try SkyDiving");
+				}
+				else if (mood == "4")
+				{
+					Console.WriteLine("You are in the mood for Good Food, you should go to Taco Bell");
+				}
+				else
+				{
+					Console.WriteLine("Please make a valid mood selection based on the available options.");
+				}
+
+			if (companions == "1")
+				{
+					Console.WriteLine("and you should travel in sneakers");
+				} else if (companions == "2")
+				{
+					Console.WriteLine("and you should travel in a sedan");
+				} else if (companions == "3")
+				{
+					Console.WriteLine("and you should travel in a Volkswagon bus");
+				} else if (companions == "4")
+				{
+					Console.WriteLine("and you should travel in an airplane");
+				} else
+				{
+					Console.WriteLine("Please make a valid companion selection based on the available options.");
+				}
+
+			Console.WriteLine("Hope you have a wonderful adventure, Goodbye!");
+			Console.ReadLine();
+		}	
 	}
 }
---
+++ Real Fun Recommendations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealFunRecommendations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("  What type of activity are you in the mood for?");
+			Console.WriteLine("   1 for Action, 2 for Chilling, 3 for Danger, and 4 for Good Food");
+			int mood = int.Parse(Console.ReadLine());
+			string action = "Looks like you are in the mood for Action, you should try stock car racing ";
+			string chilling = "Looks like you are in the mood for Chilling, you should try hiking ";
+			string danger = "Looks like you are in the mood for Danger, you should try Sky Diving ";
+			string goodFood = "Looks like you are in the mood for Good Food, you should go to Taco Bell ";
+			string choice = null;
+
+			if (mood == 1)
+				{
+					choice = action;
+				}
+				else if (mood == 2)
+				{
+					choice = chilling;
+				}
+				else if (mood == 3)
+				{
+					choice = danger;
+				}
+				else if (mood == 4)
+				{
+					choice = goodFood;
+				}
+				else
+				{
+					Console.WriteLine("Please make a valid mood selection based on the available options.");
+				}
+	
+			Console.WriteLine("And how many companions will be joining you on this adventure?");
+			int companions = int.Parse(Console.ReadLine());
+
+			string companions1 = "in sneakers";
+			string companions2 = "in a sedan";
+			string companions3 = "in a Volkswagon bus";
+			string companions4 = "in an airplane";
+			string transportation = null;
+
+			if (companions == 0)
+			{
+				transportation = companions1;
+
+			} else if ((companions >= 1) && (companions <= 4))
+			{
+				transportation = companions2;
+
+			} else if ((companions >= 5) && (companions <= 10))
+			{
+				transportation = companions3;
+
+			} else if (companions >= 11)
+			{
+				transportation = companions4;
+
+			} else
+			{
+				Console.WriteLine("Please enter a numerical value for your number of companions.");
+			}
+			Console.WriteLine(choice + transportation);
+			Console.WriteLine("Hope you have a wonderful adventure, Goodbye!");
+			Console.ReadLine();
+		}	
+	}
+}
